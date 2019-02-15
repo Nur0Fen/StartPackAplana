4af70ccfaff950e064c21ae59d08ec11df95e4b1
Nur0Fen <aalex1919@mail.ru>
2019-02-15 13:40:45 +0300

    Задание 7, Добавил обработку исключений для ввода символов/знаков и.т.д

--- src/main/java/Calc/Calc.java
+++ src/main/java/Calc/Calc.java
@@ -1,7 +1,7 @@
 package Calc;
 
 import java.util.Scanner;
-
+import java.util.InputMismatchException;
 
 public class Calc {
 
@@ -13,14 +13,35 @@ public class Calc {
     public void getInput() {
         Scanner input = new Scanner(System.in);
         System.out.println("Введи первое число ");
-        a = input.nextDouble();
+        try {
+            a = input.nextDouble();
+        }
+        catch (InputMismatchException e) {
+            throw new InputMismatchException("Ошибка ввода!Неверный формат данных");
+        } catch (NullPointerException e) {
+            throw new NullPointerException("Ошибка ввода!Неверный формат данных");
+        }
         System.out.println("Выберите операцию 1-sum, 2-minus, 3-multiply, 4-devide");
-        oper = input.nextInt();
+        try {
+            oper = input.nextInt();
+        }
+         catch (InputMismatchException e) {
+        throw new InputMismatchException("Ошибка ввода!Неверный формат данных");
+        } catch (NullPointerException e) {
+        throw new NullPointerException("Ошибка ввода!Неверный формат данных");
+        }
+
         System.out.println("Введи второе число ");
-        b = input.nextDouble();
+        try {
+            b = input.nextDouble();
+        }
+        catch (InputMismatchException e) {
+            throw new InputMismatchException("Ошибка ввода!Неверный формат данных");
+        } catch (NullPointerException e) {
+            throw new NullPointerException("Ошибка ввода!Неверный формат данных");
+        }
         input.close();
     }
-
 
     public double sum() {
         result=a + b;
